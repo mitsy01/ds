51f1ee7244ec8cbf6140e78b6ee1639e57348ac9
mitsy01 <alexanderfigmochka19@gmail.com>
2025-05-05 13:07:55 +0300

    02

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from typing import Dict, Union, List, Annotated, Optional
 from fastapi import FastAPI, HTTPException, Query, status
 from pydantic import BaseModel, EmailStr, Field
 import uvicorn
-from models import User, Order
-
+from models import User
 
 app = FastAPI()
 default_db: List[User] = []
@@ -17,16 +16,14 @@ def get_emailuser(email: EmailStr = Query(...,description="Електронна 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Користувача не знайдено.")
 
 
-@app.post("/users/", response_model=User)
+@app.post("/users/")
 def user_created(user: User):
     for ex_user in default_db:
         if ex_user.email == user.email:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Користувача з такою електронною поштою не знайдено.")
-        default_db.append(user)
-        return "Користувача додано."
+    default_db.append(user)
+    return "Користувача додано."
     
-
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=8001)
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class Order(BaseModel):
-    count: Annotated[str, Field(..., description="Кількість товару не може бути 0.", gt=0)]
+    count: Annotated[int, Field(1 , description="Кількість товару не може бути 0.", gt=0)]
     product_name: Annotated[str, Field(..., description="Назва товару не може бути порожньою.", min_length=2)]
     price: Annotated[float, Field(..., description="Ціна повинна бути більше 0.", gt=0)]
     date_created: Annotated[datetime, Field(..., default_factory=datetime.now(), description="Дата створення товару.")]
